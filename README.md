c7b4693ef3590417ca40b5511cfb789d2bf655c5
Timo <jootimo@gmail.com>
2023-05-18 16:44:50 +0300

    ex3.10-3.11

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 https://fullstackopen.com/en/part3
+
+https://phonebook-backend-ebla.onrender.com
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('person', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
