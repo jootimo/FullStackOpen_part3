f7e9f82bfa0bfe024bad1d9e768171e36a91faa6
Timo <jootimo@gmail.com>
2023-05-18 11:49:32 +0300

    ex3.1

---
+++ phonebook/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+
+let persons = [
+    {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+    },
+    {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
+    },
+    {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+    },
+    {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Phonebook</h1>')
+  })
+  
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+  
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+app.use(unknownEndpoint)
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
