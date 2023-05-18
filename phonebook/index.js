ca7d1eef05622179afd01a161540d5f5219f91b5
Timo <jootimo@gmail.com>
2023-05-18 12:03:13 +0300

    ex3.3

--- phonebook/index.js
+++ phonebook/index.js
@@ -5,24 +5,24 @@ app.use(express.json())
 
 let persons = [
     {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
     },
     {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 2
     },
     {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 3
     },
     {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4
     }
 ]
 
@@ -38,7 +38,17 @@ app.get('/info', (req, res) => {
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
-  
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person  = persons.find(note => note.id === id)
+    if(person) {
+        res.json(person)
+    } else {
+        res.status(404)
+           .send({ error: `No person found with id ${id}` })
+    }  
+})
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
